800bf5fe1fc06f424cad5ce26a84cde55dae804e
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-02 22:53:23 +0900

    Footer add

---
+++ components/Footer.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+
+export default function Footer() {
+    return (
+      <footer className="text-gray-600 body-font bg-blue-500">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <Link href={'/'} passHref>
+            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+              <span className="ml-3 text-xl text-white">KONERU-CHILDCARE</span>
+            </a>
+          </Link>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+              <a className="mr-5 text-white hover:text-gray-900">Twitter</a>
+              <a className="mr-5 text-white hover:text-gray-900">Facebook</a>
+              <a className="mr-5 text-white hover:text-gray-900">Dribbble</a>
+              <a className="mr-5 text-white hover:text-gray-900">GitHub</a>
+          </nav>
+        </div>
+      </footer>
+    )
+  }
