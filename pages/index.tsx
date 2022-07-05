e32fb0258725307eefb78bb777c261278170a011
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-05 10:09:01 +0900

    card style

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,18 @@ export default function Home({ articles }: Props) {
       <h1 className="container mx-auto px-10 pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 text-2xl font-bold">
       Sleep record
       </h1>
-      <div className="container mx-auto p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+      <div className="container mx-auto p-3 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
       {articles.map(article => (
-        <div className="rounded overflow-hidden shadow-lg" key={article.id}>
+        <div className="rounded-xl overflow-hidden shadow-lg" key={article.id}>
           <img
             className="w-full"
             src={article.eye_catch.url}
             alt="Sunset in the mountains"
           />
-          <div className="px-6 py-4">{article.title}</div>
-          <div className="px-6 pt-4 pb-2">
+          <div className="p-3 py-3">{article.title}</div>
+          <div className="p-3 ">
           {article.tag && (
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <span className="inline-block rounded-full text-sm font-semibold card-tag mr-2">
               #{article.tag}
             </span>
             )}
