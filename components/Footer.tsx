b5402bf1ffb244d92598ddea19e457fac0fecda3
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-13 11:32:11 +0900

    Footer mark up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,19 +4,26 @@ import * as Icon from 'react-feather';
 export default function Footer() {
     return (
       
-      <footer className="dark:bg-gray-800 bg-white text-gray-600 body-font bg-blue-500">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <footer className="footer dark:bg-gray-800 bg-white text-gray-600 body-font">
+        <div className="container mx-auto">
           <Link href={'/'} passHref>
-            <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <span className="ml-3 text-xl text-white">logo</span>
+            <a className="logo-wrap title-font font-medium text-gray-900 md:mb-0">
+              <span className="text-xl text-white">logo</span>
             </a>
           </Link>
-          <nav className="dark:text-white md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5" target={"_blank"}><Icon.Twitter /></a></Link>
-            <Link href={'https://www.facebook.com/w3cdpmori'}><a className="mr-5" target={"_blank"}><Icon.Facebook /></a></Link>
-            <Link href={'https://dribbble.com/w3cdp6084'}><a className="mr-5" target={"_blank"}><Icon.Dribbble /></a></Link>
-            <Link href={'https://github.com/w3cdp6084-dev'}><a className="mr-5" target={"_blank"}><Icon.GitHub /></a></Link>
+          <nav className="dark:text-white flex flex-wrap items-center text-base justify-center">
+            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5 text-white" target={"_blank"}><Icon.Twitter /></a></Link>
+            <Link href={'https://www.facebook.com/w3cdpmori'}><a className="mr-5 text-white " target={"_blank"}><Icon.Facebook /></a></Link>
+            <Link href={'https://dribbble.com/w3cdp6084'}><a className="mr-5 text-white " target={"_blank"}><Icon.Dribbble /></a></Link>
+            <Link href={'https://github.com/w3cdp6084-dev'}><a className="text-white" target={"_blank"}><Icon.GitHub /></a></Link>
           </nav>
+          <div className='links'>
+            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5 text-white dark:text-white" target={"_blank"}>koneru</a></Link>
+            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5 text-white dark:text-white" target={"_blank"}>koneru</a></Link>
+            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5 text-white dark:text-white" target={"_blank"}>koneru</a></Link>
+            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5 text-white dark:text-white" target={"_blank"}>koneru</a></Link>
+          </div>
+          <p className='copy text-white dark:text-white'>&copy; koneru childcare.</p>
         </div>
       </footer>
     )
