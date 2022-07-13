adf18afa7035d6c12c7825526cf31c08f4b40243
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-13 10:54:26 +0900

    icon add

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import * as Icon from 'react-feather';
+
 export default function Footer() {
     return (
       
@@ -6,14 +8,14 @@ export default function Footer() {
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link href={'/'} passHref>
             <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-              <span className="ml-3 text-xl text-white">KONERU-CHILDCARE</span>
+              <span className="ml-3 text-xl text-white">logo</span>
             </a>
           </Link>
           <nav className="dark:text-white md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5" target={"_blank"}>Twitter</a></Link>
-            <Link href={'https://www.facebook.com/w3cdpmori'}><a className="mr-5" target={"_blank"}>Facebook</a></Link>
-            <Link href={'https://dribbble.com/w3cdp6084'}><a className="mr-5" target={"_blank"}>Dribbble</a></Link>
-            <Link href={'https://github.com/w3cdp6084-dev'}><a className="mr-5" target={"_blank"}>GitHub</a></Link>
+            <Link href={'https://twitter.com/w3cdp6084w3cdp'}><a className="mr-5" target={"_blank"}><Icon.Twitter /></a></Link>
+            <Link href={'https://www.facebook.com/w3cdpmori'}><a className="mr-5" target={"_blank"}><Icon.Facebook /></a></Link>
+            <Link href={'https://dribbble.com/w3cdp6084'}><a className="mr-5" target={"_blank"}><Icon.Dribbble /></a></Link>
+            <Link href={'https://github.com/w3cdp6084-dev'}><a className="mr-5" target={"_blank"}><Icon.GitHub /></a></Link>
           </nav>
         </div>
       </footer>
