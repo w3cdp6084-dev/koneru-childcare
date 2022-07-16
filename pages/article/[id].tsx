d1a838dbda9d3fee418626753f953e947b29ce79
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-16 12:36:20 +0900

    detail tag  修正

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -10,10 +10,20 @@ export default function Article({ article }: Props) {
   return (
     <div>
       <div className="px-10 py-6 mx-auto">
+
         <div className="main-img max-w-6xl px-10 py-6 mx-auto">
-            <Moment format="YYYY/MM/DD" className='text-xs font-bold date'>
-                {article.publishedAt}
-            </Moment>
+        <div className='flex items-center justify-between'>
+          {article.tag && (
+              <div className="flex items-center justify-start mt-4 mb-4">
+                <div className="px-2 py-1 font-bold bg-red-400 text-white rounded-lg">
+                  #{article.tag}
+                </div>
+              </div>
+            )}
+          <Moment format="YYYY/MM/DD" className='text-xs font-bold date'>
+                  {article.publishedAt}
+          </Moment>
+        </div>
           <img
             className="rounded-2xl object-cover w-full shadow-sm h-full"
             src={article.eye_catch.url}
@@ -23,13 +33,7 @@ export default function Article({ article }: Props) {
               {article.title}
             </div>
           </div>
-          {article.tag && (
-            <div className="flex items-center justify-start mt-4 mb-4">
-              <div className="px-2 py-1 font-bold bg-red-400 text-white rounded-lg">
-                #{article.tag}
-              </div>
-            </div>
-          )}
+
           <div className="mt-2">
             <div dangerouslySetInnerHTML={{ __html: article.content }} />
           </div>
