a0c76ee2f7cc8a1a82d2eed7d6bddd583d50f923
w3cdp6084-dev <yusukew3@gmail.com>
2022-07-23 09:35:53 +0900

    test directly

---
+++ components/molecules/Pickups/index.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+export default function Pickup() {
+  return (
+    <main className="mt-20 main-img text-center px-10 py-6 mx-auto">
+      <section>
+          <div>
+              <p className='dark:text-white mt-10 mb-10 text-black'>
+              KONEKONEという屋号で、デザイナーとフロントエンドをやってます。<br />
+ブランディング/コンセプトメイクの設計を大切にし、モノづくりをおこなっています。<br />
+今年子供が生まれ、育児に楽しく取り組んでおり、こちらはそのnoteにもなっています。<br /><br />
+少しでも参考になれば幸いです🐱
+              </p>
+          </div>
+      </section>
+    </main>
+  );
+}
