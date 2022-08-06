7622cca58dbf545e3447fcfdcf5297325768b985
w3cdp6084-dev <yusukew3@gmail.com>
2022-08-06 14:20:28 +0900

    carousel temporary solution

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Moment from 'react-moment'
 import Link from 'next/link';
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 type Props = {
   articles: Array<Article>;
   pickups: Array<Pickups>;
@@ -17,7 +18,8 @@ export default function Home({ articles,pickups,recommend }: Props) {
     <div>
       <section>
         <h1 className="dark:text-white block-title container mx-auto pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 text-2xl font-bold">Pick up</h1>
-        <div className='container mx-auto pt-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3'>
+        <div className='container mx-auto pt-10 grid'>
+        <Carousel>
           {pickups.map(pickups => (
           <Link href={`/pickups/${pickups.id}`} passHref>
             <div className="dark:bg-gray-800 rounded-xl overflow-hidden shadow-lg" key={pickups.id}>
@@ -29,7 +31,7 @@ export default function Home({ articles,pickups,recommend }: Props) {
             </div>
           </Link>
           ))}
-
+        </Carousel>
         </div>
 
       </section>
